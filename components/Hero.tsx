3ce73a8938b8348e9420cbeb22070b99984d183b
Gathiira <gathiiramwangi@gmail.com>
2023-07-11 22:28:13 +0300

    upd: initial setup from gist

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Hero = () => {
+
+    return (
+        <div>
+            Hello Hero
+        </div>
+    )
+}
+
+export default Hero
