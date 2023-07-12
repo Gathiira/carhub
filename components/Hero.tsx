96f3ea1b4b9663a686ea3567ae0ead9a331139d4
Gathiira <gathiiramwangi@gmail.com>
2023-07-12 12:19:54 +0300

    upd: squoosh images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
             </div>
             <div className='hero__image-container'>
                 <div className='hero__image'>
-                    <Image src="/hero.png" alt='' fill className='object-contain' />
+                    <Image src="/hero.avif" alt='' fill className='object-contain' />
                 </div>
                 <div className='hero__image-overlay' />
             </div>
